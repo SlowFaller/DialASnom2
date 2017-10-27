3c651468b4cbff2250130b80e8dddd89239017b6
Richard Ortiz <mrortiz@ucdavis.edu>
2017-10-27 00:09:47 -0700

    main test

---
+++ DialASnom2/DialASnom2/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	string input;
+
+	cout << "Hello World!" << endl;
+	cin >> input;
+
+	return 0;
+}
